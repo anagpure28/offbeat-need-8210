751633864f0edad373cb6663492dd969006c32c3
Aniket Nagpure <aniketnagpure28@gmail.com>
2023-02-22 13:05:38 +0530

    Carousel1000

--- liberfm/src/Components/HomePage/Home.jsx
+++ liberfm/src/Components/HomePage/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "../Styles/Home.css"
 import {Box, Text} from "@chakra-ui/react"
 import BootCarousel from './BootCarousel';
+import Carousel1000 from "./Carousel1000";
 
 const Home = () => {
   return (
@@ -13,6 +14,15 @@ const Home = () => {
           <Text style={{color:"white",fontSize:"18px"}}>Free Shipping on Orders of $40 or More</Text>
         </Box>
         <BootCarousel />
+        <Text className='p1'>FIND YOUR PLACE AT B&N'S ONLINE BOOKSTORE</Text>
+        <Text>Over 5 million books ready to ship, 3.6 million eBooks and 300,000 audiobooks to download right now! Curbside pickup available in most stores!</Text>
+        <Text className='p2'>Curbside pickup available in most stores!</Text>
+        <hr style={{width:"90%",margin:"auto"}}/>
+        <Carousel1000 />
+
+
+
+
     </div>
   )
 }
